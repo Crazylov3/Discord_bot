02b25e58148b2a6ea06eff757b4e65f8cfe31a01
Crazylov3 <73377942+Crazylov3@users.noreply.github.com>
2021-07-06 12:56:19 +0700

    Update init_photo_service.py

--- cogs/Google_API/init_photo_service.py
+++ cogs/Google_API/init_photo_service.py
@@ -4,6 +4,6 @@ from cogs.Google_API.Google import Create_Service
 API_NAME = 'photoslibrary'
 API_VERSION = 'v1'
 CLIENT_SECRET_FILE = 'cogs/Google_API/Client_secret.json'
-SCOPES = ['https://photos.app.goo.gl/Krn5hNzstqugQ541A','https://www.googleapis.com/auth/photoslibrary']
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
 
 service = Create_Service(CLIENT_SECRET_FILE,API_NAME, API_VERSION, SCOPES)
