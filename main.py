e330a93840cc9b7389b9a0c55fa305d07c97a4c0
Crazylov3 <73377942+Crazylov3@users.noreply.github.com>
2021-07-15 12:47:52 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 from dotenv import load_dotenv
 from discord_slash import SlashCommand
 import discord
-from discord.ext import commands,tasks
+from discord.ext import commands
+
 
 load_dotenv()
-client = commands.Bot(command_prefix=".",activity =discord.Activity(type=discord.ActivityType.listening, name="music"))
+
+client = commands.Bot(command_prefix=".",activity =discord.Activity(name="Test", type=5))
 slash = SlashCommand(client, sync_commands=True, sync_on_cog_reload=True)
 
 
@@ -14,11 +16,28 @@ async def on_ready():
     for filename in os.listdir("cogs"):
         if filename.endswith(".py"):
             client.reload_extension(f"cogs.{filename[:-3]}")
+
     print("Bot is ready")
-    
+
+@client.event
+async def on_message(msg):
+    if msg.channel.id == 865092377664028703 and msg.channel.name == "announce":
+        if "Crazylov3:" in msg.content:
+            ls = msg.content.split(":")
+            title = ls[1]
+            text = ls[2]
+            for guild in client.guilds:
+                general = discord.utils.find(lambda x: x.name == 'general', guild.text_channels)
+                if general and general.permissions_for(guild.me).send_messages:
+                    await general.send(embed=discord.Embed(title=title,description=text))
+
+
+
 @client.command()
 async def ping(ctx):
-    await ctx.reply(f"Ping: {round(client.latency*1000)} ms",mention_author=False)
+    await ctx.reply(f"Ping: {round(client.latency * 1000)} ms", mention_author=False)
+
+
 
 
 @client.command()
@@ -26,7 +45,7 @@ async def unload(ctx, extension):
     try:
         client.unload_extension(f"cogs.{extension}")
     except:
-        await ctx.channel.send(f"Can not find extension : {extension}")
+        await ctx.send(f"Can not find extension : {extension}")
 
 
 @client.command()
@@ -34,11 +53,11 @@ async def load(ctx, extension):
     try:
         client.load_extension((f"cogs.{extension}"))
     except:
-        await  ctx.channel.send(f"Can not find extension: {extension}")
+        await  ctx.send(f"Can not find extension: {extension}")
 
 
 for filename in os.listdir("cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run(os.getenv("TOKEN"))
+client.run("ODYwNzM1MTQ1MjcwMDUwODE2.YN_kEg.N_b_zp2Sf8xbBFoC4iClmWm8ToE")
