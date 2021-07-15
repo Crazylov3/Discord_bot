66b19824c008fb796448a15ef9794ca9f3cdb714
Crazylov3 <73377942+Crazylov3@users.noreply.github.com>
2021-07-15 18:13:06 +0700

    Create main.py

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ async def on_message(msg):
                 general = discord.utils.find(lambda x: x.name.lower() == 'general' or x.name.lower() == 'chung', guild.text_channels)
                 if general and general.permissions_for(guild.me).send_messages:
                     await general.send(embed=discord.Embed(title=title,description=text))
+    else:
+        await client.process_commands(msg)
 
 
 
